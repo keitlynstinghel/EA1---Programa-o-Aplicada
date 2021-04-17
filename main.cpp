aa04c91da7819895c37b98497b7c624e2e9ff380
keitlynstinghel <>
2021-04-17 17:55:03 +0000

    Associação entre teclas e números

--- main.cpp
+++ main.cpp
@@ -5,5 +5,15 @@
 using namespace std;
 
 #define numeros 8
-#define posicoes 4
+#define teclas 4
 
+teclado char [numeros] [teclas] = {
+	{ ' a ' , ' b ' , ' c ' , ' \ 0 ' },
+	{ ' d ' , ' e ' , ' f ' , ' \ 0 ' },
+	{ ' g ' , ' h ' , ' i ' , ' \ 0 ' },
+	{ ' j ' , ' k ' , ' l ' , ' \ 0 ' },
+	{ ' m ' , ' n ' , ' o ' , ' \ 0 ' },
+	{ ' p ' , ' q ' , ' r ' , ' s ' },
+	{ ' t ' , ' u ' , ' v ' , ' \ 0 ' },
+	{ ' w ' , ' x ' , ' y ' , ' z ' }
+};
